690fd5cef85ef6074c8e0ce884038cf3ead46b50
AishIam <76727546+AishIam@users.noreply.github.com>
2022-03-08 18:06:36 +0530

    Test files added. Yet to be implemented.

---
+++ Test/lse_test_functions.h
@@ -0,0 +1,46 @@
+/*****************************************************************************************************
+** FILENAME       :    lse_test_functions.h
+**
+** DESCRIPTION    :    Header File of the lse_test.c
+**
+** Revision History   :    V_1.0
+**
+** DATE
+** ------------------------------------------------------------------------------------------------
+** 08 MAR 2022               
+** 
+** 
+** Copyright © 2022 Capgemini Group All Rights Reserved
+**
+*****************************************************************************************************/
+
+#ifndef __LSE_TESTFUNCTIONS_H__
+#define __LSE_TESTFUNCTIONS_H__
+
+/************************** Header Files **************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#include <CUnit.h>
+#include <Basic.h>
+
+#include <utility.h>
+
+/****************************** Test Function Prototypes ***************************************/
+
+//for [LSE]/01-2
+extern void Mytestfunction_textSeach(void);      //if the files containing text are found
+extern void Mytestfunction_textSearch2(void);	//if files containing the text are not found
+
+//for [LSE]/01-3
+extern void Mytestfunction_fileSearch(void);     //if file name/location is found
+extern void Mytestfunction_fileSearch2(void);    //if file name/location is not found
+
+//for [LSE]/01-4
+extern void Mytestfunction_func(void);           //if correct file name/location is provided then display contents
+extern void Mytestfunction_func2(void);			//if incorrect file name/location is provided.
+
+#endif
---
+++ Test/lse_test_main.c
@@ -0,0 +1,118 @@
+/*****************************************************************************************************
+** FILENAME       :    lse_test_main.c
+**
+** DESCRIPTION    :    Main function to call the CUNIT Framework
+**
+** Revision History   :    V_1.0
+**
+** DATE
+** ------------------------------------------------------------------------------------------------
+** 08 MAR 2022               
+** 
+** 
+** Copyright © 2022 Capgemini Group All Rights Reserved
+**
+*****************************************************************************************************/
+
+/************************** Header Files **************************************/
+
+#include <utility.h>
+#include <lse_test_functions.h>
+
+/******************************************************************************
+ Function     : init_lse
+ Description  : Initializes the suite
+ Input        : Nothing
+ Output       : int
+***************************************************************************/
+int init_lse(void)
+{
+    return 0;
+}
+
+/******************************************************************************
+ Function     : clean_lse
+ Description  : clean the suite after successful run
+ Input        : Nothing
+ Output       : int
+***************************************************************************/
+int clean_lse(void)
+{
+    return 0;
+}
+
+/*************************************************************************************
+ Function     : main
+ Description  :This is the main function to test backend functions in utility.c
+ Input        : None
+ Output       : int
+************************************************************************************/
+
+int main(int argc, char *argv[])
+{
+    CU_pSuite pSuite = NULL;
+
+    /* Register  */
+    if (CUE_SUCCESS != CU_initialize_registry())
+       return CU_get_error();
+
+    /* Add init and cleanup handlers */
+    pSuite = CU_add_suite("LOCAL SEARCH ENGINE SUITE",init_lse, clean_lse);
+    if (NULL == pSuite)
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Add test case */
+    
+    //TODO: ADD COMMENTS AND MORE TEST RUNNERS IF NEEDED
+    
+    if(NULL == CU_add_test(pSuite,"Mytestfunction_textSeach_ID_1", Mytestfunction_textSearch))
+    {
+       CU_cleanup_registry();
+        return CU_get_error();
+    }
+    
+    if(NULL == CU_add_test(pSuite,"Mytestfunction_textSearch2_ID_2", Mytestfunction_textSearch2))
+    {
+       CU_cleanup_registry();
+        return CU_get_error();
+    }
+    
+    if(NULL == CU_add_test(pSuite,"Mytestfunction_fileSearch_ID_3", Mytestfunction_fileSearch))
+    {
+       CU_cleanup_registry();
+        return CU_get_error();
+    }
+    
+    if(NULL == CU_add_test(pSuite,"Mytestfunction_fileSearch2_ID_4", Mytestfunction_fileSearch2))
+    {
+       CU_cleanup_registry();
+        return CU_get_error();
+    }
+    
+   
+    if(NULL == CU_add_test(pSuite,"Mytestfunction_func_ID_5", Mytestfunction_func))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+    
+    
+    if(NULL == CU_add_test(pSuite,"Mytestfunction_func_ID_6", Mytestfunction_func2))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+	
+
+    /* Set the mode and  invoke test(s) */ 
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+
+    /* unregister and do cleanup */
+    CU_cleanup_registry(); 
+    
+    return CU_get_error();
+}
