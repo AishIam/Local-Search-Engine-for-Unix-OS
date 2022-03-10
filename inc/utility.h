3ba6677a203f6c40795daa34798d0f5741f46f35
shonesunil <shonesunil999@gmail.com>
2022-03-10 22:36:00 +0530

    Added 4th function

--- inc/utility.h
+++ inc/utility.h
@@ -37,5 +37,6 @@ extern GSList *list;
 extern GSList* textSearch();
 extern int fileSearch();
 extern void populatePaths(char* base_path,char* result, char* path);
+extern int searchList(GSList *wordMatch);
 
 #endif
